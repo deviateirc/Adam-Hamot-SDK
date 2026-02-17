6c7bf9d2ba7eb0df9306e54a90706c0fc1eed8e7
Adam Hamot <adam@zamn.net>
2026-02-17 13:55:36 -0600

    Publish configuration

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { LotRClient } from "./client";
+export { Movie } from "./models/movie";
+export { Quote } from "./models/quote";
+export type { IMovie, MovieKey, MovieListResponse } from "./schemas/movie";
+export type {
+  Quote as IQuote,
+  QuoteKey,
+  QuoteListResponse,
+} from "./schemas/quote";
+export type { ListParams } from "./schemas";
